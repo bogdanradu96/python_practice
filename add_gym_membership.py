b4eaecdef00e047043172f7f3fdef963cfef779c
bogdanradu96 <radubogdan996@gmai.com>
2025-02-06 15:26:05 +0200

    For homework 7

--- add_gym_membership.py
+++ add_gym_membership.py
@@ -1,17 +1,25 @@
 def add_gym_membership():
-    '''
-    Add new membership package
+    """
+    Requests details to add a new membership package
+
+    Parameters:
+    -----------
+    name : str
+    duration : int
+    price : float
+    benefits : str
 
     Examples:
     ----------
     add_gym_membership()
-    '''
+    """
 
     name = input("Enter the name of the membership:")
     duration = int(input("Enter the duration of the membership in days:"))
     price = float(input("Enter the price in $:"))
     benefits = input("Enter the benefits - 1 for no benefits, 2 for One or more benefits:")
-    membership_package = {}
+    membership_packages = []
+    #Checking if benefits need to be added in the offer
     if int(benefits) == 1:
 
         benefits = "No benefits included"
@@ -22,13 +30,12 @@ def add_gym_membership():
             'benefits' :benefits
         }
         print("Membership added!", membership)
-        membership_package.update(membership)
+        membership_packages.append(membership)
 
     else:
-        benefits = []
+        benefits = [] #Creates a list for benefits in case more are added
         while True:
             benefit = input("Enter the benefit:")
-
             if benefit == "":
                 break
 
@@ -42,5 +49,11 @@ def add_gym_membership():
             'benefits': benefits
         }
         print("Membership added!", membership)
-        membership_package.update(membership)
-    print("Available memberships", membership_package)
+        membership_packages.append(membership)
+
+    #Checking if the user wants to add another offer
+    more = input("Would you like to add another membership?")
+    if more == 'Yes':
+        add_gym_membership()
+
+    print("Available memberships:", membership_packages)
