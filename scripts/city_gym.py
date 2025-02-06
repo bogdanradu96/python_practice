e9014ada571f16fafb4e41244409391987094b93
bogdanradu96 <radubogdan996@gmai.com>
2025-02-06 17:43:19 +0200

    Pentru rulare module proiect

--- scripts/city_gym.py
+++ scripts/city_gym.py
@@ -7,5 +7,7 @@ if __name__ == "__main__":
     task = input("What would you like to do? 1 - add new membership, 2 - check availability:\n")
     if task == "1":
         gym.add_gym_membership()
-    else:
+    elif task == "2":
         check.membership_check()
+    else:
+        print("That option is not available")
