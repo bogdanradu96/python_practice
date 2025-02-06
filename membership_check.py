a191180e7d11df6bca87db516e7ea8d1edabc321
bogdanradu96 <radubogdan996@gmai.com>
2025-02-06 17:28:53 +0200

    Pentru tema 8

--- membership_check.py
+++ membership_check.py
@@ -1,10 +1,13 @@
 from datetime import datetime
-#o mica verificare poate cu comprehension sau ternar
-#un print cu f
 
 def membership_check():
     current_date = datetime.now().date()
     creation_date = datetime.strptime(input("Enter the date when the membership was created (YYYY-MM-DD):"), "%Y-%m-%d").date()
-    duration = int(input("Enter the duration of the membership in days"))
-    print("Diferenta", current_date - creation_date)
-    print("Your membership has expired" if (current_date - creation_date).days > duration else "Membership available") #Operator ternar
+    duration = int(input("Enter the duration of the membership in days:"))
+    remaining_time = (current_date - creation_date).days
+    expired = True if (current_date - creation_date).days > duration else False #Operator ternar
+
+    if expired is True:
+        print("Your membership has expired")
+    else:
+        print(f"Your membership is still available and you have {remaining_time} days left.") #Output formatting with f-string
