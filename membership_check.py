d2aab186a42c1c0fa40cf3afc83492a2c7b61ea4
bogdanradu96 <radubogdan996@gmai.com>
2025-02-06 17:49:59 +0200

    Pentru tema 8

--- membership_check.py
+++ membership_check.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
 def membership_check():
-    current_date = datetime.now().date()
-    creation_date = datetime.strptime(input("Enter the date when the membership was created (YYYY-MM-DD):"), "%Y-%m-%d").date()
+    current_date = datetime.now().date() #Get the system date as a date format from datetime
+    creation_date = datetime.strptime(
+        input("Enter the date when the membership was created (YYYY-MM-DD):"),
+        "%Y-%m-%d").date() #Convert the user provided date from str to date format
     duration = int(input("Enter the duration of the membership in days:"))
     remaining_time = duration - (current_date - creation_date).days
     expired = True if (current_date - creation_date).days > duration else False #Operator ternar
