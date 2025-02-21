e9723a35da6c317857427571eca3c3af841a90f5
bogdanradu96 <radubogdan996@gmai.com>
2025-02-21 10:50:18 +0200

    Updated indentation

--- scripts/city_gym.py
+++ scripts/city_gym.py
@@ -14,17 +14,17 @@ if __name__ == "__main__":
     class TaskException(Exception):
         pass
 
-try:
-    task = input("What would you like to do? 1 - add new membership, 2 - check availability, 3 - view memberships:\n")
-    if task == "1":
-        gym.add_gym_membership()
-    elif task == "2":
-        creation_date = input("Enter the date when the membership was created (YYYY-MM-DD):")
-        duration = int(input("Enter the duration of the membership in days:"))
-        print(check.membership_check(creation_date, duration))
-    elif task == "3":
-        view.view_memberships()
-    else:
-        raise TaskException #We raise the self-made exception if the input is incorrect
-except TaskException:
-    print("Option not available")
+    try:
+        task = input("What would you like to do? 1 - add new membership, 2 - check availability, 3 - view memberships:\n")
+        if task == "1":
+            gym.add_gym_membership()
+        elif task == "2":
+            creation_date = input("Enter the date when the membership was created (YYYY-MM-DD):")
+            duration = int(input("Enter the duration of the membership in days:"))
+            print(check.membership_check(creation_date, duration))
+        elif task == "3":
+            view.view_memberships()
+        else:
+            raise TaskException #We raise the self-made exception if the input is incorrect
+    except TaskException:
+        print("Option not available")
